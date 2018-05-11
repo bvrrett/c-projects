32abcd66b9e26a6a2ebeaf195bab2ccc71f63ce6
bvrrett <36489681+bvrrett@users.noreply.github.com>
2018-05-11 21:54:48 +0000

    Update contact.c

--- contact.c
+++ contact.c
@@ -1,36 +1,47 @@
 /*
- * Contact.c
+ * Person.h
  *
- *  Created on: May 10, 2018
+ *  Created on: Apr 24, 2018
  *      Author: spencerbarrett
  */
 
-#include "Contact.h"
+#ifndef CONTACT_H_
+#define CONTACT_H_
 
-Contact * AddContact(int contactIndex, char *pContactInfo) {
-	Contact * pContactNewContact = (Contact *) malloc(sizeof(Contact));
-	if (pContactNewContact)
-	{
-		strcpy(pContactNewContact->contactInfo, pContactInfo);
-//		pContactNewContact->pPrev = NULL;
-//		pContactNewContact->pNext = NULL;
-		if (contactIndex == 0){
-			pContactNewContact->pPrev = NULL;
-			pContactNewContact->pNext;
-			return pContactNewContact;
-		}else{
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-		}
+/*
+ * STRUCTURE DEFINITIONS
+ */
+typedef struct contact Contact;
+typedef struct contact* pContact;
+struct contact{
+	char contactInfo[2000];
+	char lastName[250];
+	char firstName[250];
+	char emailAddress[250];
+	char phoneNumber[250];
+	Contact *pPrev;
+	Contact *pNext;
+	Contact *pHead;
+};
 
-	}
-	return pContactNewContact;
-}
+/*
+ * FUNCTION DECLARATIONS
+ */
 
-//Contact * GetContact(int *pContactIndex) {
-//	int i;
-//	pContactIndex = (Contact *) malloc(sizeof(Contact));
-//
-//	for(i = 0; i < pContactIndex; i++){
-//
-//	}
-//}
+Contact *AddContact(int contactIndex, char *contactInfo);
+
+Contact *DeleteContact(int contactIndex);
+
+Contact *GetContact(int contactIndex);
+
+Contact *GetField(int contactIndex, char *contactInfo);
+
+
+
+
+
+#endif /* PERSON_H_ */
